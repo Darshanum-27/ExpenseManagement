5a41ac765c70d36ea01a0c6f936c3b66eb468c06
Darshanum-27 <darshanum27@gmail.com>
2024-09-02 20:17:31 -0500

    First commit message

---
+++ Demo/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Demo.Models
+{
+	public class Expense
+	{
+		public int Id { get; set; }
+		public decimal Value { get; set; }
+		[Required]
+		public string Description { get; set; }
+	}
+}
+
---
+++ Demo/Models/SpendSmartDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace Demo.Models
+{
+	public class SpendSmartDBContext: DbContext
+	{
+		public DbSet<Expense> Expenses { get; set; }
+		public SpendSmartDBContext(DbContextOptions<SpendSmartDBContext> options): base(options)
+		{
+
+		}
+	}
+}
+
